4a21f6bb41e158cf4103b13864018ac6aa606e97
Vineet Naik <naikvin@gmail.com>
2024-02-09 16:45:06 +0530

    Move the index i.e. '/' route out of the flask app
    To simulate an SPA, the index page is added as a standalone html.

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,8 @@
-from flask import Flask, request, make_response, redirect, url_for, render_template
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
 USERS = {"vineet": "s3cret"}
-
-
-@app.route("/")
-def index():
-    user = request.cookies.get("logged_in_user")
-    data = {}
-    if user:
-        data["name"] = user
-        data["is_logged_in"] = True
-    else:
-        data["name"] = "guest"
-        data["is_logged_in"] = False
-    return render_template("index.html", **data)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -25,7 +12,7 @@ def login():
     else:
         username = request.form.get("username")
         if username and username in USERS and request.form.get("password") == USERS[username]:
-            resp = make_response(redirect(url_for("index"), 302))
+            resp = make_response(redirect("/", 302))
             resp.set_cookie("logged_in_user", username)
             return resp
         else:
@@ -34,7 +21,7 @@ def login():
 
 @app.route("/logout", methods=["GET"])
 def logout():
-    resp = make_response(redirect(url_for("index"), 302))
+    resp = make_response(redirect("/", 302))
     resp.delete_cookie("logged_in_user")
     return resp
 

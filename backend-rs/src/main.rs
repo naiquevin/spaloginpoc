c57fb4753fe6cddc08ec35ae2112ec2ed2c7a76a
Vineet Naik <naikvin@gmail.com>
2024-02-15 11:00:09 +0530

    Replace the root route with /login route
    The minijinja crate has been introduced as a dependency for rendering
    the login form.

--- backend-rs/src/main.rs
+++ backend-rs/src/main.rs
@@ -1,15 +1,31 @@
+use axum::extract::State;
+use axum::response::Html;
 use axum::routing::get;
 use axum::Router;
+use minijinja::{context, path_loader, Environment};
 use tokio::net::TcpListener;
 
-async fn root() -> &'static str {
-    "Hello, World!"
+#[derive(Clone)]
+pub struct AppState {
+    pub tmpl_env: Environment<'static>,
+}
+
+async fn login(State(state): State<AppState>) -> Html<String> {
+    let tmpl = state.tmpl_env.get_template("login.html").unwrap();
+    let ctx = context!();
+    Html(tmpl.render(ctx).unwrap())
 }
 
 #[tokio::main]
 async fn main() {
+    let mut tmpl_env = Environment::new();
+    tmpl_env.set_loader(path_loader("templates"));
+
+    let state = AppState { tmpl_env };
+
     let app = Router::new()
-        .route("/", get(root));
+        .route("/login", get(login))
+        .with_state(state);
 
     let addr = String::from("0.0.0.0:5001");
     println!("Starting HTTP server listening on {}", addr);
